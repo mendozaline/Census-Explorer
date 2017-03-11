8c2fe7fb110227ca20f0630099365d37c3b174cd
David Mendoza <mendoza.line@aim.com>
2017-03-10 20:49:54 -0800

    Import and render Map component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Map from './components/Map.jsx'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+    }
+  } //end constructor
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
+      <div>
+        <h1>Census Data Visualizer</h1>
+        <Map />
       </div>
     )
   }
