a68944c5a0eb94c800c817ab87826a660da513e8
David Mendoza <mendoza.line@aim.com>
2017-03-12 00:39:42 -0800

    Update variable names

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,38 +11,40 @@ var projection = d3.geoConicConformal()
   .precision(.1)
 
 var path = (props) => {
-  console.log('MAP props:', props)
+  //console.log('MAP props:', props)
   return d3.geoPath().projection(projection)
 }
 
 var findMin = (data) => {
   return d3.min(data, (d) => {
-//    console.log(d.properties.value)
-    return d.properties.value
+    //console.log(MAP d.properties.apiValue)
+    return d.properties.apiValue
   })
 }
 
 var findMax = (data) => {
   return d3.max(data, (d) => {
-//    console.log(d.properties.value)
-    return d.properties.value
+    //console.log(MAP d.properties.apiValue)
+    return d.properties.apiValue
   })
 }
 
 var color = (props) => {
-//  console.log('min:', findMin(props.json))
-//  console.log('max:', findMax(props.json))
+  console.log('MAP min:', findMin(props.apiJSON))
+  console.log('MAP max:', findMax(props.apiJSON))
   return d3.scaleQuantize()
     .domain([
-      findMin(props.json),
-      findMax(props.json)
+      findMin(props.apiJSON),
+      findMax(props.apiJSON)
     ])
-    .range(['#feebe2','#fbb4b9','#f768a1','#ae017e'])
+    .range(['#fff7f3','#fde0dd','#fcc5c0',
+            '#fa9fb5','#f768a1','#dd3497',
+            '#ae017e','#7a0177','#49006a'])
 }
 
 export default (props) => {
-  if (props.data2 === null) {
-    return <p>Loading...</p>
+  if (props.apiJSON === null) {
+    return <p>MAP: Waiting for a variable...</p>
   }
 
   const scales = {
@@ -50,7 +52,7 @@ export default (props) => {
     path: path(props),
   }
 
-  console.log('MAP props', props)
+//  console.log('MAP props', props)
 
   return (
     <svg width={props.width} height={props.height}>
