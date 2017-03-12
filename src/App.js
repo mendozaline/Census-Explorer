23b4c8367213ed02da2829d0e023c678755c91e5
David Mendoza <mendoza.line@aim.com>
2017-03-12 00:38:31 -0800

    Add drop-down lists to select variable

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ class App extends Component {
     super()
 
     this.callAPI = this.callAPI.bind(this)
+    this.dropdownSelect = this.dropdownSelect.bind(this)
+    this.dropdownClick = this.dropdownClick.bind(this)
 
     this.state = {
       apiJSON: null,
@@ -81,14 +83,62 @@ class App extends Component {
 //    this.callAPI()
 //  }
 
+  dropdownSelect(event) {
+    event.preventDefault()
+    //console.log('APP dds evt.target.value:', event.target.value)
+    this.setState({
+      censusVariable: event.target.value
+    })
+  }
+
+  dropdownClick() {
+    //console.log('APP ddc censusVar: ', this.state.censusVariable)
+    this.callAPI()
+  }
+
   render() {
 
     console.log('APP apiJSON:', this.state.apiJSON)
     console.log('APP cenVar:', this.state.censusVariable)
 
+    const censusVarCodes = [
+      {name: '', varCode: ''},
+      {name: 'WHITE ALONE', varCode: 'B01001A_001E'},
+      {name: 'BLACK OR AFRICAN AMERICAN ALONE', varCode: 'B01001B_001E'},
+      {name: 'AMERICAN INDIAN AND ALASKA NATIVE ALONE', varCode: 'B01001C_001E'},
+      {name: 'ASIAN ALONE', varCode: 'B01001D_001E'},
+      {name: 'NATIVE HAWAIIAN AND OTHER PACIFIC ISLANDER ALONE', varCode: 'B01001E_001E'},
+      {name: 'SOME OTHER RACE ALONE', varCode: 'B01001F_001E'},
+      {name: 'TWO OR MORE RACES', varCode: 'B01001G_001E'},
+      {name: 'WHITE ALONE, NOT HISPANIC OR LATINO', varCode: 'B01001H_001E'},
+      {name: 'HISPANIC OR LATINO', varCode: 'B01001I_001E'},
+    ]
+
+    let censusVarDropdown = censusVarCodes.map((cenVarObj, index) => {
+      //console.log('APP cenVarObj', cenVarObj)
+      return (
+        <option key={index} value={cenVarObj.varCode}>
+          {cenVarObj.name}
+        </option>
+      )
+    })
+
     return (
       <div>
         <h1>Census Data Visualizer</h1>
+        <div>
+          <select
+            value={this.state.censusVariable}
+            onChange={this.dropdownSelect} >
+            {censusVarDropdown}
+          </select>
+
+          <input
+            type='submit'
+            value='Send'
+            onClick={this.dropdownClick} />
+        </div>
+
         <Map {...this.state} {...styles} />
 
       </div>
