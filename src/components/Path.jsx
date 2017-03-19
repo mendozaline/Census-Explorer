a543c825a92b6c2eb045f86a7acac156e219db4c
David Mendoza <mendoza.line@aim.com>
2017-03-18 17:06:55 -0700

    Refactor into single component

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -1,163 +1,125 @@
-import React, { Component } from 'react'
-//import React from 'react'
+import React from 'react'
 import * as d3 from 'd3'
-import styled from 'styled-components'
+import PathCounties from './PathCounties.jsx'
 
-const Path = styled.path`
-  &:hover {
-    stroke: dodgerBlue;
-    stroke-width: 5.5px;
-  }
-`
+export default (props) => {
+  console.log('PATH props:', props)
+  //console.log('PATH props.apiJSON:', props.apiJSON)
 
-const renderPaths = (props) => {
-  console.log('PATH props rdpth:', props)
-  return (stateObj, index) => {
-    //console.log('PATH stateObj.prop:', stateObj.properties)
+  return (
+    <g>
+      {
+        props.apiJSON.map( (stateObj, index) => {
+          //console.log('i', index)
 
-    let strokeColor = null
-    if (stateObj.properties.NAME === 'Coconino') {
-      strokeColor = 'gainsboro'
-    } else {
-      strokeColor = 'gainsboro'
-    }
+          let fillColor = null
+          if (isNaN(stateObj.properties.apiValue)) {
+            fillColor = 'white'
+          } else {
+            fillColor = props.color(stateObj.properties.apiValue)
+          }
 
-    let strokeWidth = null
-    if (stateObj.properties.NAME === 'Coconino') {
-      strokeWidth = .75
-    } else {
-      strokeWidth = .75
-    }
+          let tooltipOn = function(event) {
+            //console.log('name: ', stateObj.properties.NAME)
+            //console.log('apiValue: ', stateObj.properties.apiValue)
+            //console.log('pageX:', event.pageX)
+            //console.log('index:', index)
 
-    let fillColor = null
-    if (isNaN(stateObj.properties.apiValue)) {
-      fillColor = 'white'
-    } else {
-      fillColor = props.color(stateObj.properties.apiValue)
-    }
+            let pageXAdjustment = null
+            if (event.pageX > 450) {
+              pageXAdjustment = event.pageX - 250
+            } else {
+              pageXAdjustment = event.pageX + 45
+            }
 
-    let tooltipOn = function(event) {
-      //console.log('name: ', stateObj.properties.NAME)
-      //console.log('apiValue: ', stateObj.properties.apiValue)
-      //console.log('pageX:', event.pageX)
-//      console.log('index:', index)
+            d3.select('#tooltip')
+              .style('left', pageXAdjustment + 'px')
+              .style('top', (event.pageY - 45) + 'px')
 
-      let pageXAdjustment = null
-      if (event.pageX > 450) {
-        pageXAdjustment = event.pageX - 250
-      } else {
-        pageXAdjustment = event.pageX + 45
+            d3.select('#value1')
+              .text(stateObj.properties.countyState)
+
+            d3.select('#value2')
+              .text(stateObj.properties.apiValue)
+
+            d3.select('#tooltip').classed('hidden', false);
+          } //end tooltipOn
+
+          let tooltipOff = function() {
+            d3.select('#tooltip').classed('hidden', true);
+          } //end tooltipOff
+
+          let centered;
+          let zoomIn = function() {
+            //console.log('p', props.path.centroid(stateObj))
+            console.log('PATH i', index)
+
+            let x, y, k
+            //if clicked county not prev. clicked
+            if (stateObj && centered !== stateObj) {
+              console.log('zoom IN!')
+              //console.log('PATHG stateObj', stateObj)
+              //console.log('PATHG ccentered', centered)
+
+              let centroid = props.path.centroid(stateObj)
+              let b = props.path.bounds(stateObj)
+              //console.log('PATHG bounds', b)
+              x = centroid[0]
+              y = centroid[1]
+              k = 0.8 / Math.max((b[1][0] - b[0][0]) / props.width,
+                                 (b[1][1] - b[0][1]) / props.height)
+              centered = stateObj
+            } else {
+              console.log('zoom OUT!')
+              x = props.width / 2
+              y = props.height / 2
+              k = 1
+              centered = null
+            }
+
+//            d3.select(stateObj)
+//              .classed('highlight')
+//              .classed('active', function(stateObj) {
+//              //console.log('classed cent', centered)
+//              return stateObj !== centered;
+//            })
+//              .style('stroke', 'crimson')
+//              .style('fill', function() {
+//              //console.log('if stObj': stateObj)
+//              if (stateObj === stateObj) {
+//                //console.log('yes!')
+//                return 'dodgerBlue';
+//              } else {
+//                return 'crimson'
+//              }
+//            })
+
+            d3.selectAll('path')
+              //.transition()
+              //.duration(500)
+              .attr('transform', 'translate(' + props.width / 2 + ',' + props.height / 2 + ')scale(' + k + ')translate(' + -x + ',' + -y + ')')
+              .style('stroke-width', 0.75 / k + 'px');
+
+          } //end zoom
+
+          const pathProps = {
+            d: props.path(stateObj),
+            fill: fillColor,
+            stroke: 'gainsboro',
+            strokeWidth: 0.75,
+            key: index,
+            onMouseOver: tooltipOn,
+            onMouseLeave: tooltipOff,
+            onClick: zoomIn,
+          }
+
+          return (
+            <PathCounties
+              {...props}
+              {...pathProps} />
+          )
+        })
       }
-
-      d3.select('#tooltip')
-        .style('left', pageXAdjustment + 'px')
-        .style('top', (event.pageY - 45) + 'px')
-
-      d3.select('#value1')
-        .text(stateObj.properties.countyState)
-
-      d3.select('#value2')
-        .text(stateObj.properties.apiValue)
-
-      d3.select('#tooltip').classed('hidden', false);
-    } //end tooltipOn
-
-    let tooltipOff = function() {
-      d3.select('#tooltip').classed('hidden', true);
-    } //end tooltipOff
-
-    let centered;
-
-    let zoomIn = function() {
-//      console.log('p', props.path.centroid(stateObj))
-      let x, y, k
-      //if clicked county not prev. clicked
-      if (stateObj && centered !== stateObj) {
-        console.log('zoom IN!')
-        console.log('s', stateObj)
-        console.log('c', centered)
-
-        let centroid = props.path.centroid(stateObj)
-        let b = props.path.bounds(stateObj)
-//        console.log('b', b)
-        x = centroid[0]
-        y = centroid[1]
-        k = 0.8 / Math.max( (b[1][0] - b[0][0]) / props.width,
-                           (b[1][1] - b[0][1]) / props.height)
-        centered = stateObj
-      } else {
-        console.log('OUT!')
-        x = props.width / 2
-        y = props.height / 2
-        k = 1
-        centered = null
-      }
-
-//      d3.select(stateObj)
-//        .classed('highlight')
-//        .classed('active', function(stateObj) {
-////          console.log('classed cent', centered)
-//          return stateObj !== centered;
-//        })
-//        .style('stroke', 'crimson')
-//        .style('fill', function() {
-////          console.log('if stObj': stateObj)
-//          if (stateObj === stateObj) {
-//            console.log('yes!')
-//            return 'dodgerBlue';
-//          } else {
-//            return 'crimson'
-//          }
-//        })
-
-      d3.selectAll('path')
-//        .transition()
-//        .duration(500)
-        .attr("transform", "translate(" + props.width / 2 + "," + props.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-        .style("stroke-width", 0.75 / k + "px");
-    }
-
-    const pathProps = {
-      d: props.path(stateObj),
-      fill: fillColor,
-      stroke: strokeColor,
-      strokeWidth: strokeWidth,
-      key: index,
-      onMouseOver: tooltipOn,
-      onMouseLeave: tooltipOff,
-      onClick: zoomIn,
-      //className: 'counties',
-    }
-
-    return (
-      <Path {...pathProps} >
-        {/* <title>
-          {stateObj.properties.NAME} County: {stateObj.properties.apiValue}
-        </title> */}
-      </Path>
-    )
-  }
-} //end renderPaths
-
-
-
-
-
-export default class PathG extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-    } //end state
-  } //end constructor
-
-  render() {
-  console.log('PATH props.apiJSON!:', this.props.apiJSON)
-
-    return(
-      <g>
-        { this.props.apiJSON.map(renderPaths(this.props)) }
-      </g>
-    ) //end return
-  } //end render
+    </g>
+  ) //end return
 } //end Path Counties
