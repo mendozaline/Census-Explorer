ede2b2e1fdb0d3bbf2ab64c06ceeb8d624b25a1a
David Mendoza <mendoza.line@aim.com>
2017-03-18 17:01:29 -0700

    Add PathCounties components

---
+++ src/components/PathCounties.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Path = styled.path`
+  &:hover {
+    stroke: dodgerBlue;
+    stroke-width: 5.5px;
+  }
+`
+
+export default class PathCounties extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    //console.log('COUNTIES props', this.props.key)
+
+    return (
+      <Path {...this.props} >
+        {/* <title>
+          {stateObj.properties.NAME} County: {stateObj.properties.apiValue}
+        </title> */}
+      </Path>
+    )
+
+  }
+}
+
+
+
